18190a57061f99e33c2051906c25a60fd85b4b7b
Marccudi <marccudi@hotmail.es>
2022-08-01 10:56:05 +0200

    Ejercicio 7

--- ud2002/src/main/java/team4/ud2002/AppGrafic.java
+++ ud2002/src/main/java/team4/ud2002/AppGrafic.java
@@ -14,21 +14,6 @@ public class AppGrafic {
 
 	public JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					AppGrafic window = new AppGrafic();
-//					window.frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
 
 	/**
 	 * Create the application.
--- ud2007/src/main/java/team4/ud2007/App.java
+++ ud2007/src/main/java/team4/ud2007/App.java
@@ -1,5 +1,7 @@
 package team4.ud2007;
 
+import team4.ud2007.AppGrafic;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	AppGrafic window = new AppGrafic();
+		window.frame.setVisible(true);
     }
 }
