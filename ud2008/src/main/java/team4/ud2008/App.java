9ab0b8e03413b76b0a46c25f2486391e5191ded1
Jimxx22 <jaumegil22@gmail.com>
2022-08-01 09:25:42 +0200

    UD2008 Project

---
+++ ud2008/src/main/java/team4/ud2008/App.java
@@ -0,0 +1,13 @@
+package team4.ud2008;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
